0322df13ab8edb34563bb9e3a97a539a4798c5b4
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-08 12:55:25 +0300

    login form tracking changes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,3 +1,43 @@
+import { useState } from "react";
+
 export default function LoginForm() {
-  return;
+  const [data, setData] = useState({ username: "", password: "" });
+
+  function handleChange(e) {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div className="login-form">
+      <h1>login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          {" "}
+          USERNAME
+          <input
+            name="username"
+            onChange={handleChange}
+            type="text"
+            value={data.username}
+          />
+        </label>
+        <label>
+          {" "}
+          password
+          <input
+            name="password"
+            onChange={handleChange}
+            type="password"
+            value={data.password}
+          />
+        </label>
+        <button>submit</button>
+      </form>
+    </div>
+  );
 }
