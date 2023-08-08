512e694f724cc26cd66481b8420ef4f96b16ac93
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-08 12:56:23 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import LoginForm from "./components/LoginForm";
 import FriendList from "./components/FriendList";
 import AddFriend from "./components/AddFriend";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
@@ -12,7 +13,18 @@ function App() {
         <Navigation />
       </header>
 
-      <LoginForm />
+      <div className="content">
+        <Route exact path="/">
+          <LoginForm />
+        </Route>
+
+        <Route path="/friends">
+          <FriendList />
+        </Route>
+        <Route path="/add">
+          <AddFriend />
+        </Route>
+      </div>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
-  return;
-  <nav className="navbar">
-    <NavLink to="/login">LOGIN.</NavLink>
-    <NavLink to="/friends">FRIENDLIST.</NavLink>
-    <NavLink to="/add">ADDFRIEND</NavLink>
-    <p>LOGOUT.</p>
-  </nav>;
+  return (
+    <nav className="navbar">
+      <NavLink to="/">LOGIN.</NavLink>
+      <NavLink to="/friends">FRIENDLIST.</NavLink>
+      <NavLink to="/add">ADDFRIEND.</NavLink>
+      <a>LOGOUT.</a>
+    </nav>
+  );
 }
