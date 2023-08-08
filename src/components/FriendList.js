ead14e37d4bf7b70f5835b8d904caaabd7ec7757
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-08 14:34:11 +0300

    friend list from axios data visible

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,3 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
+import { lsKey } from "./LoginForm";
+
 export default function FriendList() {
-  return;
+  const [friends, setFriends] = useState([]);
+  const token = localStorage.getItem(lsKey);
+  const baseURL = "http://localhost:9000/api/friends";
+  const api = axios.create({
+    baseURL,
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+  });
+
+  api
+    .get(baseURL)
+    .then((res) => setFriends(res.data))
+    .catch((err) => console.log(err));
+
+  return (
+    <div>
+      <h1>friends list</h1>
+      {friends.map((friend) => (
+        <p key={friend.id}>
+          {friend.name} - {friend.email}
+        </p>
+      ))}
+    </div>
+  );
 }
